af3a371a9c62f245d85cf3c3ea387bc50bcb778a
Mohammad Islam <Atikul10152002@gmail.com>
2018-11-23 16:30:08 -0500

    more layers

--- dream_image.py
+++ dream_image.py
@@ -27,15 +27,15 @@ from deepdreamer import model, load_image, recursive_optimize
 import numpy as np
 import PIL.Image
 
-layer_tensor = model.layer_tensors[11]
+layer_tensor = model.layer_tensors[6]
 file_name = "input/input.jpg"
 img_result = load_image(filename='{}'.format(file_name))
 
 img_result = recursive_optimize(layer_tensor=layer_tensor, image=img_result,
                  # how clear is the dream vs original image        
-                 num_iterations=100, step_size=1.0, rescale_factor=.8,
+                 num_iterations=40, step_size=1.0, rescale_factor=.8,
                  # How many "passes" over the data. More passes, the more granular the gradients will be.
-                 num_repeats=25, blend=0.2)
+                 num_repeats=10, blend=0.2)
 
 img_result = np.clip(img_result, 0.0, 255.0)
 img_result = img_result.astype(np.uint8)
